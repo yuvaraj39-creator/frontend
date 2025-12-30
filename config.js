4e039a47782831f42dc92f468f702b9f1bb0630e
Yuvaraj <yuva3929@gmail.com>
2025-12-30 16:42:26 +0530

    loby updated

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.AppConfig = {
-    API_BASE_URL: "https://backend-svvg.onrender.com",
-    // API_BASE_URL: "http://localhost:3000",
+    // API_BASE_URL: "https://backend-svvg.onrender.com",
+    API_BASE_URL: "http://localhost:3000",
 };
